ef54993791202a211be78931e76c1a63828e9bf3
wangw <wangwenshuo228@163.com>
2022-05-01 23:18:07 +0800

    2022-04-30

--- src/main/java/com/ahr/film/servlet/RecommendServlet.java
+++ src/main/java/com/ahr/film/servlet/RecommendServlet.java
@@ -1,21 +1,33 @@
 package com.ahr.film.servlet;
 
+import com.ahr.film.entity.Film;
+import com.ahr.film.entity.User;
+import com.ahr.film.recomandation.Recommendation;
+import com.alibaba.fastjson.JSONArray;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "RecommenServlet", value = "/RecommenServlet")
 public class RecommendServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         this.doPost(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ServletUtils.setCharSet(request, response);
-
+        User u = new User();
+        u.setUserId(Integer.parseInt(request.getParameter("userId")));
+        List<Film> filmList = Recommendation.getRecommendFilmList(u);
+        JSONArray arr = new JSONArray();
+        for(Film f : filmList){
+            arr.add(f.toString());
+        }
+        response.getWriter().println(arr);
     }
 }
